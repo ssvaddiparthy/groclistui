ded8c6599038611db524e7d862ddfed87e58ecf4
vaddipar <vaddipar@adobe.com>
2020-01-07 14:39:20 +0530

    Table appears

--- src/App.js
+++ src/App.js
@@ -2,27 +2,48 @@ import React, {Component} from 'react'
 import './App.css'
 import axios from 'axios'
 
+
+class FooBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (this.props.rocks.map(function (i, j) {
+            return <tr key={j}>
+                <td>{i.name}</td>
+                <td>{i.amount}</td>
+                <td>{i.unit}</td>
+            </tr>
+        }))
+
+
+    }
+}
+
 class App extends Component {
     weekDays = ["Sunday", "Monday"]
     meals = ["BreakFast"]
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipeList: [],
-        selectedRecipes: [],
-        fetchedJson: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipeList: [],
+            selectedRecipes: [],
+            fetchedJson: {},
+            fooBar: false,
+            showTable: true
+        }
     }
-  }
 
-  renderTableHeader() {
-    return this.meals.map((key, mealIndex) => {
-      return <th key={mealIndex}>{key.toUpperCase()}</th>
-    })
-  }
+    renderTableHeader() {
+        return this.meals.map((key, mealIndex) => {
+            return <th key={mealIndex}>{key.toUpperCase()}</th>
+        })
+    }
 
-  componentDidMount() {
-    axios.get(`http://localhost:8080/recipe/all`)
+    componentDidMount() {
+        axios.get(`http://localhost:8080/recipe/all`)
         .then(res => {
           this.setState({ recipeList: res.data });
           // todo: line here will work before lines outside the axios.get
@@ -31,7 +52,6 @@ class App extends Component {
 
   handleChange(event){
     event.preventDefault();
-    console.log(event.target.value);
     this.state.selectedRecipes.push(event.target.value);
   }
 
@@ -65,38 +85,50 @@ class App extends Component {
 
   handleSubmit(event){
       event.preventDefault();
-      console.log(this.state.recipeList);
       let url = 'http://localhost:8080/ingredients/?recipes=' + this.state.selectedRecipes.join(',')
-    axios.get(url)
-        .then(res => {
-          console.log(res.data);
-          this.setState({
-              fetchedJson: res.data
+      axios.get(url)
+          .then(res => {
+              this.setState({
+                  fetchedJson: res.data,
+                  fooBar: true
+              })
           })
-        })
       this.setState({
-          selectedRecipes: []
-      })
+          selectedRecipes: [],
+          showTable: false
+      });
   }
 
   render() {
       return (
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <table id='students' border="1">
-            <tbody>
-            <tr>
-              <td><b>WeekDay</b></td>
+          <div>
               {
-                this.renderTableHeader()
-              }
-            </tr>
-            {this.renderTableData()}
-            </tbody>
-          </table>
-          <input type="submit" value="Submit" />
-        </form>
+                  this.state.showTable && <form onSubmit={this.handleSubmit.bind(this)}>
+                      <table id='students' border="1">
+                          <tbody>
+                          <tr>
+                              <td><b>WeekDay</b></td>
+                              {
+                                  this.renderTableHeader()
+                              }
+                          </tr>
+                          {this.renderTableData()}
+                          </tbody>
+                      </table>
+                      <input type="submit" value="Submit"/>
 
-    )
+                  </form>
+              }
+              {this.state.fooBar &&
+              <table border="1">
+                  <tbody>
+                  <FooBar rocks={this.state.fetchedJson}/>
+                  </tbody>
+              </table>
+              }
+          </div>
+      )
   }
 }
 export default App
+
