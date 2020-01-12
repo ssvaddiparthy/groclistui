ee5363cd0f13b3520fe219785c66110cc8496178
vaddipar <vaddipar@adobe.com>
2020-01-12 06:18:13 +0530

    working after refactor

--- src/ResultTable.js
+++ src/ResultTable.js
@@ -5,45 +5,49 @@ export class ResultTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fetchedJson: [],
-      fooBar: false
+      fetchedJson: {},
+      isLoaded: false
     };
   }
 
+  componentDidMount() {
+    this.handleSubmit(this.props.recipeList);
+  };
+
   handleSubmit(selectedRecipes) {
-    if (!this.state.fooBar) {
+    if (!this.state.isLoaded) {
       let url =
-        "http://localhost:8080/ingredients/?recipes=" +
-        selectedRecipes.join(",");
-      console.log(url);
+          "http://localhost:8080/ingredients/?recipes=" +
+          selectedRecipes.join(",");
       axios.get(url).then(res => {
         this.setState({
           fetchedJson: res.data,
-          fooBar: true
+          isLoaded: true
         });
       });
     }
   }
 
   render() {
-    this.handleSubmit(this.props.recipeList);
-    return (
-      <div>
-        <h1 align="center">Ingredients</h1>
-        <table border="1" align="center">
-          <tbody>
-            {this.state.fetchedJson.map(function(ingredient, ingredientIndex) {
+    return this.state.isLoaded ? (
+        <div>
+          <h1 align="center">Ingredients</h1>
+          <table border="1" align="center">
+            <tbody>
+            {JSON.parse(this.state.fetchedJson.responseData).map(function(ingredient, ingredientIndex) {
               return (
-                <tr key={ingredientIndex}>
-                  <td>{ingredient.name}</td>
-                  <td>{ingredient.amount}</td>
-                  <td>{ingredient.unit}</td>
-                </tr>
+                  <tr key={ingredientIndex}>
+                    <td>{ingredient.name}</td>
+                    <td>{ingredient.amount}</td>
+                    <td>{ingredient.unit}</td>
+                  </tr>
               );
             })}
-          </tbody>
-        </table>
-      </div>
+            </tbody>
+          </table>
+        </div>
+    ) : (
+        <div>Loading...</div>
     );
   }
 }
